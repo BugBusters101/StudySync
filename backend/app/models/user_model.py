6610c8e593e54fdb3d851457cf7020555f020e36
Shreyas Satpute <satputeshreyas07@gmail.com>
2025-03-22 16:26:10 +0000

    Updated views,models and controller

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -1,14 +1,15 @@
 
 import sqlalchemy.orm as so
 import sqlalchemy as sqla
-from app import db, login
+from StudySync.backend.app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model, UserMixin):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sqla.String(64), index=True, unique=True)
+    firstname: so.Mapped[str] = so.mapped_column(sqla.String(64), index=True, unique=True)
+    lastname: so.Mapped[str] = so.mapped_column(sqla.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sqla.String(128), index=True, unique=True)
     password_hash: so.Mapped[str] = so.mapped_column(sqla.String(256))
 
