7caec1c8758ac201ff5348c47f079cb20572f60c
gagan <gagankaur1709@gmail.com>
2025-03-22 21:54:15 +0000

    Link login with backend

--- study-sync-frontend/src/Pages/SignUp.js
+++ study-sync-frontend/src/Pages/SignUp.js
@@ -24,7 +24,7 @@ const handleSubmit = async (e) => {
 
       const data = await response.json();
       if (response.ok) {
-        navigate('/preferences');
+        navigate('/dashboard');
       } else {
         setError(data.message);
       }
